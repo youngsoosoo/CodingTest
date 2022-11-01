c0b7be7048661dbb03957591d5a6069fb6e98e6c
youngsoosoo <lovepys01@naver.com>
2022-11-01 22:29:04 +0900

    백준 단계별 풀이 : 2차원 배열

--- Main.java
+++ Main.java
@@ -1,19 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt() * sc.nextInt() * sc.nextInt();
-        int b[] = new int[10];
+        int a = sc.nextInt();
+        int[][] c = new int[100][100];
         int count = 0;
 
-        while (a>0){
-            b[a%10]++;
-            a /= 10;
-        }
-        for(int i=0; i<10; i++){
+        for(int i=0; i<a; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
 
+            for(int j=x; j<x+10;j++){
+                for(int k=y; k<y+10; k++){
+                    c[j][k] = 1;
+                }
+            }
         }
+
+        for(int i=0; i<100; i++){
+            for(int j=0; j<100; j++){
+                if(c[i][j]==1){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
--- sugardelivery.java
+++ sugardelivery.java
@@ -5,13 +5,13 @@ public class sugardelivery {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        if(N==4 || N==7){
+        if(N==4 || N==7){               // 4, 7일땐 무조건 -1
             System.out.println(-1);
-        }else if (N%5 == 0) {
+        }else if (N%5 == 0) {           // 5로 나눈 나머지가 0이라면 N/5
             System.out.println(N/5);
-        }else if(N%5 == 1 || N%5 == 3){
+        }else if(N%5 == 1 || N%5 == 3){ // 5로 나눈 나머지가 1과 3이라면 N/5에서 1을 더해준다.
             System.out.println(N/5 + 1);
-        }else if(N%5 == 2 || N%5 == 4){
+        }else if(N%5 == 2 || N%5 == 4){ // 5로 나눈 나머지가 2와 4이라면 N/5에서 2를 더해준다.
             System.out.println(N/5 + 2);
         }
     }
